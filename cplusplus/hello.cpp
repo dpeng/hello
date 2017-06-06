7394dfe7837889990fafc1d428766d9b99900858
U-NSN-INTRA\dpeng <dpeng@CNU40391YZ.nsn-intra.net>
2017-06-06 09:11:47 +0800

    add new hello world of C plus plus

---
+++ cplusplus/hello.cpp
@@ -0,0 +1,12 @@
+/*****************************************
+*	gcc hello.cpp -lstdc++ -o hello.exe
+******************************************/
+
+#include <iostream>
+using namespace std;
+int main(void)
+{
+     cout<<"Hello!\n";
+     cout<<"Welcome to c++!\n";
+	 return 0;
+}

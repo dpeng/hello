e8b523b1dc917fc075035688e679278d77aa1b1e
dpeng <dongfeng.peng@nokia-sbell.com>
2021-03-24 10:27:37 +0800

    replace single tag to tuple

--- python/myshare/myshare.py
+++ python/myshare/myshare.py
@@ -13,68 +13,56 @@ def get_data_print():
     pd.set_option('display.max_columns', None)
     formatSpace = '  '
     for num in range(0,300):
-        rtData = ts.get_realtime_quotes(['sh', 'sz', '002739', '002244', '600814', '000156']) 
-
+        rtData = ts.get_realtime_quotes(['sh', 'sz', '002739', '002244', '600814', '000156','601658']) 
         rtDataFormart = rtData[['code','time','open', 'pre_close','price','bid','ask','volume','amount','date']]
 
-        if num == 0 :
-            print(rtDataFormart)
-        arrays = pd.DataFrame(rtDataFormart.to_numpy())
-        #print(arrays)
-        wddyCurrentPrice  = float(arrays[4][2])
-        bjjtCurrentPrice  = float(arrays[4][3])
-        hzjbCurrentPrice  = float(arrays[4][4])
-        hscmCurrentPrice  = float(arrays[4][5])
-        wddyPreClosePrice = float(arrays[3][2])
-        bjjtPreClosePrice = float(arrays[3][3])
-        hzjbPreClosePrice = float(arrays[3][4])
-        hscmPreClosePrice = float(arrays[3][5])
-        currentTime       =  arrays[1][1]
-        currentDate       =  arrays[9][1]
-
-        wddyShareCount  = 1200.00
-        bjjtShareCount  = 2800.00
-        hzjbShareCount  = 2500.00
-        hscmShareCount  = 2000.00
-        myAccountleft   = 33738.95
+        myAccountleft   = 17302.20
         investCount     = 100000.00
+        stockName       = ('wddy', 'bjjt', 'hzjb', 'hscm', 'ycyh')
+        shareCount      = (1200.00, 2800.00, 2500.00, 2000.00, 2800.00)
+        currentPrice    = [0.00, 0.00, 0.00, 0.00, 0.00]
+        preClosePrice   = [0.00, 0.00, 0.00, 0.00, 0.00]
+        vibratePrecent  = [0.00, 0.00, 0.00, 0.00, 0.00]
+        TotalShare      = 0.00
+        ToadyBenefit    = 0.00
+        TotalBenifit    = 0.00
 
-        wddyPrecent = (wddyCurrentPrice - wddyPreClosePrice)/wddyPreClosePrice
-        bjjtPrecent = (bjjtCurrentPrice - bjjtPreClosePrice)/bjjtPreClosePrice
-        hzjbPrecent = (hzjbCurrentPrice - hzjbPreClosePrice)/hzjbPreClosePrice
-        hscmPrecent = (hscmCurrentPrice - hscmPreClosePrice)/hscmPreClosePrice
+        #if num == 0 :
+        #    print(rtDataFormart)
+        arrays = pd.DataFrame(rtDataFormart.to_numpy())
+        currentTime     =  arrays[1][1]
+        currentDate     =  arrays[9][1]
+        
+        for i in range (0,5):
+            currentPrice[i]   = float(arrays[4][i+2])
+            preClosePrice[i]  = float(arrays[3][i+2])
+            vibratePrecent[i] = (currentPrice[i] - preClosePrice[i])/preClosePrice[i]
+            TotalShare       += currentPrice[i]*shareCount[i]
+            ToadyBenefit     += (currentPrice[i] - preClosePrice[i]) * shareCount[i]
+        TotalShare           += myAccountleft
+        TotalBenifit          = TotalShare - investCount
 
-        TotalShare   = wddyCurrentPrice * wddyShareCount + \
-                       bjjtCurrentPrice * bjjtShareCount + \
-                       hzjbCurrentPrice * hzjbShareCount + \
-                       hscmCurrentPrice * hscmShareCount + \
-                       myAccountleft
-        ToadyBenefit = (wddyCurrentPrice - wddyPreClosePrice) * wddyShareCount + \
-                       (bjjtCurrentPrice - bjjtPreClosePrice) * bjjtShareCount + \
-                       (hzjbCurrentPrice - hzjbPreClosePrice) * hzjbShareCount + \
-                       (hscmCurrentPrice - hscmPreClosePrice) * hscmShareCount
-        TotalBenifit = TotalShare - investCount
-
-        print(currentTime,  'wddy:{:.2f}%'.format(wddyPrecent*100), formatSpace ,\
-                            'bjjt:{:.2f}%'.format(bjjtPrecent*100), formatSpace ,\
-                            'hzjb:{:.2f}%'.format(hzjbPrecent*100), formatSpace ,\
-                            'hscm:{:.2f}%'.format(hscmPrecent*100), formatSpace ,\
-                            'pro:%d'%ToadyBenefit, '%d'%TotalBenifit)
+        print(currentTime,  stockName[0], '{:.2f}%'.format(vibratePrecent[0]*100), formatSpace ,\
+                            stockName[1], '{:.2f}%'.format(vibratePrecent[1]*100), formatSpace ,\
+                            stockName[2], '{:.2f}%'.format(vibratePrecent[2]*100), formatSpace ,\
+                            stockName[3], '{:.2f}%'.format(vibratePrecent[3]*100), formatSpace ,\
+                            stockName[4], '{:.2f}%'.format(vibratePrecent[4]*100), formatSpace ,\
+                            'pro {:.2f}'.format(ToadyBenefit), '{:.2f}'.format(TotalBenifit))
         time.sleep(5)
 
         # write result to file per ten times
         if num%10 == 9:
             fileName = currentDate + ".txt"
             with open(fileName,"a") as file:
-                file.write(str(arrays[1][1]) + formatSpace + \
-                    str(round(TotalShare     , 2)) + formatSpace + \
-                    str(round(wddyPrecent*100, 2)) + formatSpace + \
-                    str(round(bjjtPrecent*100, 2)) + formatSpace  + \
-                    str(round(hzjbPrecent*100, 2)) + formatSpace  +\
-                    str(round(hscmPrecent*100, 2)) + formatSpace  +\
-                    str(round(ToadyBenefit   , 2)) + formatSpace + \
-                    str(round(TotalBenifit   , 2)) + "\n")
+                file.write(str(arrays[1][1])             + formatSpace + \
+                    str(round(TotalShare     , 2))       + formatSpace + \
+                    str(round(vibratePrecent[0]*100, 2)) + formatSpace + \
+                    str(round(vibratePrecent[1]*100, 2)) + formatSpace + \
+                    str(round(vibratePrecent[2]*100, 2)) + formatSpace + \
+                    str(round(vibratePrecent[3]*100, 2)) + formatSpace + \
+                    str(round(vibratePrecent[4]*100, 2)) + formatSpace + \
+                    str(round(ToadyBenefit   , 2))       + formatSpace + \
+                    str(round(TotalBenifit   , 2))       + "\n")
 
 if __name__ == "__main__":
-    #print("start...", end="")
     get_data_print()

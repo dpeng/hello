57fb25269f7eeaf8e4379fedbc5a5bc023d1afff
dpeng <df.peng@hotmail.com>
2021-04-16 15:23:50 +0800

    add color of the result, red for positive, green for negtive

--- python/myshare/myshare.py
+++ python/myshare/myshare.py
@@ -13,13 +13,13 @@ def get_data_print():
     pd.set_option('display.max_columns', None)
     formatSpace = '  '
     for num in range(0,300):
-        rtData = ts.get_realtime_quotes(['sh', 'sz', '002739', '002244', '600814', '000156','601658']) 
+        rtData = ts.get_realtime_quotes(['sh', 'sz', '600893','600398', '000966']) 
         rtDataFormart = rtData[['code','time','open', 'pre_close','price','bid','ask','volume','amount','date']]
 
-        myAccountleft   = 17302.20
+        myAccountleft   = 44009.74
         investCount     = 100000.00
-        stockName       = ('wddy', 'bjjt', 'hzjb', 'hscm', 'ycyh')
-        shareCount      = (1200.00, 2800.00, 2500.00, 2000.00, 2800.00)
+        stockName       = ('hfdl', 'hlzj', 'cydl', '', '')
+        shareCount      = (500.00, 2600.00, 1200.00, 0.00, 0.00)
         currentPrice    = [0.00, 0.00, 0.00, 0.00, 0.00]
         preClosePrice   = [0.00, 0.00, 0.00, 0.00, 0.00]
         vibratePrecent  = [0.00, 0.00, 0.00, 0.00, 0.00]
@@ -33,21 +33,37 @@ def get_data_print():
         currentTime     =  arrays[1][1]
         currentDate     =  arrays[9][1]
         
-        for i in range (0,5):
+        print(currentTime, end=' ')
+        for i in range (0,3):
+            print(stockName[i], end='')
             currentPrice[i]   = float(arrays[4][i+2])
             preClosePrice[i]  = float(arrays[3][i+2])
             vibratePrecent[i] = (currentPrice[i] - preClosePrice[i])/preClosePrice[i]
             TotalShare       += currentPrice[i]*shareCount[i]
             ToadyBenefit     += (currentPrice[i] - preClosePrice[i]) * shareCount[i]
+            if(vibratePrecent[i] < 0.0):
+                print ('%s %s %s' % ("\033[0;32;40m", '{:.2f}%'.format(vibratePrecent[i]*100), "\033[0m"), end=' ')  #green
+            else:
+                print ('%s %s %s' % ("\033[0;31;40m", '{:.2f}%'.format(vibratePrecent[i]*100), "\033[0m"), end=' ')  #red
+            print(formatSpace, end='')
         TotalShare           += myAccountleft
         TotalBenifit          = TotalShare - investCount
-
-        print(currentTime,  stockName[0], '{:.2f}%'.format(vibratePrecent[0]*100), formatSpace ,\
-                            stockName[1], '{:.2f}%'.format(vibratePrecent[1]*100), formatSpace ,\
-                            stockName[2], '{:.2f}%'.format(vibratePrecent[2]*100), formatSpace ,\
-                            stockName[3], '{:.2f}%'.format(vibratePrecent[3]*100), formatSpace ,\
-                            stockName[4], '{:.2f}%'.format(vibratePrecent[4]*100), formatSpace ,\
-                            'pro {:.2f}'.format(ToadyBenefit), '{:.2f}'.format(TotalBenifit))
+        print("pro", end='')
+        if(ToadyBenefit < 0.0):
+            print ('%s %s %s' % ("\033[0;32;40m", '{:.2f}'.format(ToadyBenefit), "\033[0m"), end='')  #green
+        else:
+            print ('%s %s %s' % ("\033[0;31;40m", '{:.2f}'.format(ToadyBenefit), "\033[0m"), end='')  #red
+        if(TotalBenifit < 0.0):
+            print ('%s %s %s' % ("\033[0;32;40m", '{:.2f}'.format(TotalBenifit), "\033[0m"))  #green
+        else:
+            print ('%s %s %s' % ("\033[0;31;40m", '{:.2f}'.format(TotalBenifit), "\033[0m"))  #red
+        
+        #print(currentTime,  stockName[0], '{:.2f}%'.format(vibratePrecent[0]*100), formatSpace ,\
+        #                    stockName[1], '{:.2f}%'.format(vibratePrecent[1]*100), formatSpace ,\
+        #                    stockName[2], '{:.2f}%'.format(vibratePrecent[2]*100), formatSpace ,\
+        #                    stockName[3], '{:.2f}%'.format(vibratePrecent[3]*100), formatSpace ,\
+        #                    stockName[4], '{:.2f}%'.format(vibratePrecent[4]*100), formatSpace ,\
+        #                    'pro {:.2f}'.format(ToadyBenefit), '{:.2f}'.format(TotalBenifit))
         time.sleep(5)
 
         # write result to file per ten times

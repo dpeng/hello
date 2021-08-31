3614d39ebfafd246dbc8136da2dac50ca75cb044
dpeng <df.peng@hotmail.com>
2021-08-31 15:12:10 +0800

    add windoes version

---
+++ python/myshare/myshare_windows.py
@@ -0,0 +1,81 @@
+# -*- coding:utf-8         -*- 
+# -*- Created on 20201029  -*- 
+# -*- @author: Kacper      -*- 
+
+import tushare as ts
+import pandas as pd
+import time
+
+def print_with_color(var, target, flag):
+    if(var < target):
+        print ('%s%s%s%s' % ("\033[32m", '{:.2f}'.format(abs(var)), flag, "\033[0m"), end='')  #green
+        #print ('%s%s%s%s' % ("\033[34m", '{:.2f}'.format(abs(var)), flag, "\033[0m"), end='')  #blue
+    else:
+        print ('%s%s%s%s' % ("\033[31m", '{:.2f}'.format(abs(var)), flag, "\033[0m"), end='')  #red
+
+def get_data_print(_):
+    pd.set_option('display.max_columns', None)
+    currentPrice    = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+    preClosePrice   = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+    vibratePrecent  = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+    TotalShare      = 0.00
+    ToadyBenefit    = 0.00
+    TotalBenifit    = 0.00
+    rtData          = []
+    try:
+        rtData = ts.get_realtime_quotes(stockCode) 
+    except Exception as ex:
+        print("Oops!  Exception<<%s>>detected, Will ry again couple of seconds later... "%ex)
+        return
+
+    rtDataFormart = rtData[['code','time','open', 'pre_close','price','bid','ask','volume','amount','date']]
+
+    arrays = pd.DataFrame(rtDataFormart.to_numpy())
+    currentTime     =  arrays[1][1]
+    currentDate     =  arrays[9][1]
+
+    print(currentTime, '', end='')
+    i = 0
+
+    while stockCode[i] != '':
+        currentPrice[i]   = float(arrays[4][i])
+        preClosePrice[i]  = float(arrays[3][i])
+        vibratePrecent[i] = (currentPrice[i] - preClosePrice[i])/preClosePrice[i]
+        TotalShare       += currentPrice[i]*shareCount[i]
+        ToadyBenefit     += (currentPrice[i] - preClosePrice[i]) * shareCount[i]
+
+        if(i < 1):
+            #print_with_color(currentPrice[i], preClosePrice[i], '') # output szzs as refer
+            print('{:.0f}'.format(currentPrice[i]), end='')
+            print_with_color(currentPrice[i] - preClosePrice[i], 0.0, '') # output szzs as refer
+        else:
+            #print_with_color(currentPrice[i], preClosePrice[i], '_') # output currentPrice with color
+            if (countforPrint % 10):
+                print('{:.2f}'.format(currentPrice[i]), end='') #output current Price
+            else:
+                print(stockName[i], end='')
+            print_with_color(vibratePrecent[i]*100, 0.0, '%')
+        print('  ',end='')
+        i = i + 1
+    TotalShare           += myAccountleft
+    TotalBenifit          = TotalShare - investCount
+    print("pro ", end='')
+    print_with_color(ToadyBenefit, 0.0, '')
+    print(' ' ,end='')
+    print_with_color(TotalBenifit, 0.0, '')
+    print() # new line
+    time.sleep(3)
+
+
+if __name__ == "__main__":
+    # setting init account information
+    myAccountleft   = 2102.02
+    investCount     = 100000.00
+    stockName       = (''    , 'xkgf '  ,  'sagd '    ,   'syzg '      ,   'mjny '         ,   ''       ,   ''       )
+    stockCode       = ('sh'  , '601127' ,  '600703'   ,   '600031'     ,   '000723'        ,   ''       ,   ''       )
+    shareCount      = (0.00  ,  400.00  ,  600.00     ,    800.00      ,    1500.00         ,   0.00     ,   0.00     )
+    countforPrint   = 0.0
+    for i in range(300):
+        get_data_print( countforPrint )
+        countforPrint = countforPrint + 1
+

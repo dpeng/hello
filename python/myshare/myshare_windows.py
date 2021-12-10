49430827322902f50bbf4e596d83e6a74dbdaf5f
dpeng <df.peng@hotmail.com>
2021-12-10 15:23:04 +0800

    add windows version

--- python/myshare/myshare_windows.py
+++ python/myshare/myshare_windows.py
@@ -18,6 +18,7 @@ def get_data_print(_):
     currentPrice    = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
     preClosePrice   = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
     vibratePrecent  = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+    stockName       = [' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ]
     TotalShare      = 0.00
     ToadyBenefit    = 0.00
     TotalBenifit    = 0.00
@@ -28,7 +29,7 @@ def get_data_print(_):
         print("Oops!  Exception<<%s>>detected, Will ry again couple of seconds later... "%ex)
         return
 
-    rtDataFormart = rtData[['code','time','open', 'pre_close','price','bid','ask','volume','amount','date']]
+    rtDataFormart = rtData[['code','time','open', 'pre_close','price','bid','ask','volume','amount','date', 'name']]
 
     arrays = pd.DataFrame(rtDataFormart.to_numpy())
     currentTime     =  arrays[1][1]
@@ -36,32 +37,31 @@ def get_data_print(_):
 
     print(currentTime, '', end='')
     i = 0
-
-    while stockCode[i] != '':
+    while stockCode[i]   != '':
         currentPrice[i]   = float(arrays[4][i])
         preClosePrice[i]  = float(arrays[3][i])
+        stockName[i]      = arrays[10][i]
+
         vibratePrecent[i] = (currentPrice[i] - preClosePrice[i])/preClosePrice[i]
         TotalShare       += currentPrice[i]*shareCount[i]
         ToadyBenefit     += (currentPrice[i] - preClosePrice[i]) * shareCount[i]
 
         if(i < 1):
-            #print_with_color(currentPrice[i], preClosePrice[i], '') # output szzs as refer
             print('{:.0f}'.format(currentPrice[i]), end='')
             print_with_color(currentPrice[i] - preClosePrice[i], 0.0, '') # output szzs as refer
         else:
-            #print_with_color(currentPrice[i], preClosePrice[i], '_') # output currentPrice with color
-            if (countforPrint % 10):
+            if (countforPrint != 0):
                 print('{:.2f}'.format(currentPrice[i]), end='') #output current Price
+                print_with_color(vibratePrecent[i]*100, 0.0, '%')
             else:
                 print(stockName[i], end='')
-            print_with_color(vibratePrecent[i]*100, 0.0, '%')
         print('  ',end='')
         i = i + 1
     TotalShare           += myAccountleft
     TotalBenifit          = TotalShare - investCount
     print("pro ", end='')
     print_with_color(ToadyBenefit, 0.0, '')
-    print(' ' ,end='')
+    #print(' ' , end='')
     print_with_color(TotalBenifit, 0.0, '')
     print() # new line
     time.sleep(3)
@@ -69,11 +69,10 @@ def get_data_print(_):
 
 if __name__ == "__main__":
     # setting init account information
-    myAccountleft   = 2102.02
+    myAccountleft   = 149.78
     investCount     = 100000.00
-    stockName       = (''    , 'xkgf '  ,  'sagd '    ,   'syzg '      ,   'mjny '         ,   ''       ,   ''       )
-    stockCode       = ('sh'  , '601127' ,  '600703'   ,   '600031'     ,   '000723'        ,   ''       ,   ''       )
-    shareCount      = (0.00  ,  400.00  ,  600.00     ,    800.00      ,    1500.00         ,   0.00     ,   0.00     )
+    stockCode       = ('sh'  , '603893' ,  '603606'  ,   '300017'       ,   '300364'         ,   ''       ,   ''       )
+    shareCount      = (0.00  ,  200.00  ,  400.00    ,   5900.00        ,    0.00         ,   0.00     ,   0.00     )
     countforPrint   = 0.0
     for i in range(300):
         get_data_print( countforPrint )
